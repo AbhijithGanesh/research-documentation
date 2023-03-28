a99c13ec0eda368877b52632ba1a6c3421f87034
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-03-28 20:03:13 +0530

    Addition of SEO elements
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { ImLinkedin2 } from "react-icons/im"
 import 'tailwindcss/tailwind.css'
-import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 import Head from 'next/head'
 import WebsiteSEO from './components/seo'
 
@@ -20,16 +21,19 @@ const config: DocsThemeConfig = {
     icon: <ImLinkedin2 />,
     link: 'https://linkedin.com/in/AbhijithGanesh14',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  head: (
-    <>
-      <Head>
-        <meta property="title">Abhijith's research and experiements</meta>
-        <meta property='description' content="Discover a vast collection of groundbreaking research in Computer Science, compiled here.From cutting- edge studies to in -depth analysis, our repository is your one - stop destination for the latest findings and insights.Explore our resources and stay up - to - date with the latest trends and developments in CS, AI and Cloud." />      < meta property='og:title' content="Abhijith's research repository" />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
-      </Head>
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://research.AbhijithGanesh.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return <>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+      <meta property="og:description" content={frontMatter.description || 'The next site builder'} />
     </>
-  ),
+  },
   feedback: {
     content: <></>
   },

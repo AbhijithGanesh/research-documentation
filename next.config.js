d7f3aeee3572e9bffce41be3766ee4f0338a1d5f
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-09-05 05:37:23 +0000

    Addition of CD configurable content
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  images:{
-    unoptimized:true
-  },
   experimental: {
     turboMode: true,
   },
